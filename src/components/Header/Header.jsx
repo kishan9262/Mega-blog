2ea119db2d7d14f1114d522fba6de161be7487d3
Kishan Jagani <jaganikishan46@gmail.com>
2024-08-14 12:46:36 +0530

    "" add

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Logo, Container, LogoutBtn } from "../index;
+import { Logo, Container, LogoutBtn } from "../index";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
